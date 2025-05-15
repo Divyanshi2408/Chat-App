628305c18a05d760b3a36f31db5c39f2b30845e6
Divyanshi <85859444+Divyanshi2408@users.noreply.github.com>
2025-05-15 22:55:34 +0530

    add in backend

--- server/index.js
+++ server/index.js
@@ -9,6 +9,16 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
+app.use(cors({
+  origin: process.env.Origin,
+  methods: ['GET', 'POST', 'PUT','PATCH', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
+
+app.use(cookieParser());
+app.use(express.json());
+
 mongoose
   .connect(MONGO_URI, {
     useNewUrlParser: true,
---
+++ server/models/User.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import {genSalt, hash, compare} from "bcrypt";
+
+const User = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: false,
+  },
+   lastName: {
+    type: String,
+    required: false,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+   image: {
+    type: String,
+    required: "false",
+    },
+    color: {
+      type: Number,
+      required: "false",
+    },
+    profileSetup: {
+      type: Boolean,
+      default: false,
+    },
+});
+
+UserSchema.pre("save", async function (next) {
+ const salt = await genSalt(10);
+  this.password = await hash(this.password, salt);
+  next();
+});
+
+const UserSchema = mongoose.model("User", User);
+
+export default UserSchema;
