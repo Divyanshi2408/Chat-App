fbb07eb52124435d119790ee3bcc2bff535d6c31
Divyanshi <85859444+Divyanshi2408@users.noreply.github.com>
2025-06-07 23:48:13 +0530

    add frontend

--- client/src/store/Slices/auth-slice.js
+++
@@ -1,4 +0,0 @@
-export const createAuthSlice = (set, get) => ({
-  userInfo:undefined,
-  setUserInfo: (userInfo) => set({ userInfo }),
-});
--- client/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import {create} from "zustand";
-import { createAuthSlice } from "./Slices/auth-slice";
-
-export const useAppStore = create((...a) => ({
-  ...createAuthSlice(...a),
-  // Add other slices here
-  // Example: ...createOtherSlice(...a),
-}));
